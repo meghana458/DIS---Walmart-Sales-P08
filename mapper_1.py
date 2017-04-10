f1e08c014711f9ad8f0dc4e7832b77638c9af481
Meghana <s525770@mail.nwmissouri.edu>
2017-04-10 14:45:06 -0500

    Mapper & Reducer for Weekly sales during Non-holidays

---
+++ mapper_1.py
@@ -0,0 +1,28 @@
+from __future__ import division
+from itertools import groupby
+import itertools
+#import groupby
+import operator
+
+
+f = open("traindup.txt","r")  # open file, read-only raw data
+
+o = open("o.txt", "w") # open file, write - just our key, value pairs
+
+for line in f:  
+
+    data = line.strip().split("\t") 
+
+    #print data
+
+    #print len(data)
+
+    if len(data) == 5:
+
+        store, dept, date, sales, holidays = data
+
+        print "{0}\t{1}\t{2}".format(store, sales, holidays)
+
+       
+f.close()
+o.close()
