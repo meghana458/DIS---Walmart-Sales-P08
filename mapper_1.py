817165c245ae02141cd5ed6cd40a0db38daa358a
Meghana <s525770@mail.nwmissouri.edu>
2017-04-12 08:15:51 -0500

    Edited Map Reduce with comments and code change

--- mapper_1.py
+++ mapper_1.py
@@ -1,28 +1,15 @@
-from __future__ import division
-from itertools import groupby
-import itertools
-#import groupby
-import operator
+#!/usr/bin/python
 
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
 
-f = open("train.txt","r")  # open file, read-only raw data
+import sys
 
-o = open("o.txt", "w") # open file, write - just our key, value pairs
-
-for line in f:  
-
-    data = line.strip().split("\t") 
-
-    #print data
-
-    #print len(data)
-
-    if len(data) == 5:
-
-        store, dept, date, sales, holidays = data
-
-        print "{0}\t{1}\t{2}".format(store, sales, holidays)
-
-       
-f.close()
-o.close()
+for line in sys.stdin:
+    data = line.strip().split("\t") # delimit input by tab 
+    if len(data) == 5: # if length of data equal to 5
+        store, dept, date, sales, holidays = data  # assign each entry to a variable
+        print "{0}\t{1}\t{2}".format(store, sales, holidays) #delimits printed output by tab
