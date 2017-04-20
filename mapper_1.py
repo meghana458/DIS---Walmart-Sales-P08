296b691f342aac134eba3e7cbd7af9907732c7bb
meghana458 <s525770@mail.nwmissouri.edu>
2017-04-19 23:19:12 -0500

    Changed Mapper_1 with reading input from file

--- mapper_1.py
+++ mapper_1.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python
-
-# Format of each line is:
-# date\ttime\tstore name\titem description\tcost\tmethod of payment
-#
-# We want elements 2 (store name) and 4 (cost)
-# We need to write them out to standard output, separated by a tab
-
-import sys
-
-for line in sys.stdin:
-    data = line.strip().split("\t") # delimit input by tab 
-    if len(data) == 5: # if length of data equal to 5
-        store, dept, date, sales, holidays = data  # assign each entry to a variable
-        print "{0}\t{1}\t{2}".format(store, sales, holidays) #delimits printed output by tab
+f = open("train2.txt","r")  # open file, read-only raw data
+o = open("o.txt", "w") # open file, write - just our key, value pairs
+for line in f:  
+    data = line.strip().split("\t") 
+    #print data
+    #print len(data)
+    if len(data) == 5:
+        store, dept, date, sales, holidays = data
+        #print "{0}\t{1}\t{2}".format(store, sales, holidays)
+        o.write("{0}\t{1}\t{2}\n".format(store, sales, holidays))
+f.close()
+o.close()
